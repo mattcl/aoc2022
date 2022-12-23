ba22fa5493e73356e37be77dd2f57f7d6332978e
Matt Chun-Lum <matt@questionable.engineering>
2022-12-23 12:09:29 -0800

    perf(day23): use observation that collisions are from opposite dirs

--- day-023-unstable-diffusion/src/lib.rs
+++ day-023-unstable-diffusion/src/lib.rs
@@ -2,7 +2,7 @@ use std::{collections::VecDeque, hash::Hash, str::FromStr};
 
 use aoc_helpers::generic::Bound2D;
 use aoc_plumbing::Problem;
-use rustc_hash::{FxHashMap, FxHashSet};
+use rustc_hash::FxHashSet;
 
 const N_NE_NW: usize = 0b10010100;
 const S_SE_SW: usize = 0b00101001;
@@ -19,6 +19,19 @@ const NEIGHBORS: [(i16, i16); 8] = [
     (1, 1),
 ];
 
+//                              | N     | S      | W | E
+const NORTH_ORDER: [usize; 8] = [2, 4, 7, 0, 3, 5, 1, 6];
+const NORTH_CHUNKS: [usize; 4] = [3, 3, 1, 1];
+//                              | S     | W   | E   | N
+const SOUTH_ORDER: [usize; 8] = [0, 3, 5, 1, 2, 6, 7, 4];
+const SOUTH_CHUNKS: [usize; 4] = [3, 2, 2, 1];
+//                             | W     | E      | N | S
+const WEST_ORDER: [usize; 8] = [0, 1, 2, 5, 6, 7, 4, 3];
+const WEST_CHUNKS: [usize; 4] = [3, 3, 1, 1];
+//                             | E     | N   | S   | W
+const EAST_ORDER: [usize; 8] = [5, 6, 7, 2, 4, 0, 3, 1];
+const EAST_CHUNKS: [usize; 4] = [3, 2, 2, 1];
+
 #[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash)]
 pub struct Point {
     x: i16,
@@ -34,63 +47,73 @@ pub enum Choice {
 }
 
 impl Choice {
-    pub fn propose(
-        &self,
-        elf: &Point,
-        neighbors: usize,
-        proposals: &mut FxHashMap<Point, Vec<Point>>,
-    ) -> bool {
+    pub fn order_when_first(&self) -> &'static [usize] {
+        match self {
+            Self::North => &NORTH_ORDER,
+            Self::South => &SOUTH_ORDER,
+            Self::East => &EAST_ORDER,
+            Self::West => &WEST_ORDER,
+        }
+    }
+
+    pub fn chunks_when_first(&self) -> &'static [usize] {
+        match self {
+            Self::North => &NORTH_CHUNKS,
+            Self::South => &SOUTH_CHUNKS,
+            Self::East => &EAST_CHUNKS,
+            Self::West => &WEST_CHUNKS,
+        }
+    }
+
+    pub fn corresponding_mask(&self) -> usize {
+        match self {
+            Self::North => N_NE_NW,
+            Self::South => S_SE_SW,
+            Self::East => E_NE_SE,
+            Self::West => W_NW_SW,
+        }
+    }
+
+    pub fn propose(&self, elf: &Point, neighbors: usize) -> Option<Point> {
         match self {
             Self::North => {
                 if neighbors & N_NE_NW == 0 {
-                    let next = Point {
+                    Some(Point {
                         x: elf.x,
                         y: elf.y + 1,
-                    };
-                    let e = proposals.entry(next).or_default();
-                    e.push(*elf);
-                    true
+                    })
                 } else {
-                    false
+                    None
                 }
             }
             Self::South => {
                 if neighbors & S_SE_SW == 0 {
-                    let next = Point {
+                    Some(Point {
                         x: elf.x,
                         y: elf.y - 1,
-                    };
-                    let e = proposals.entry(next).or_default();
-                    e.push(*elf);
-                    true
+                    })
                 } else {
-                    false
+                    None
                 }
             }
             Self::West => {
                 if neighbors & W_NW_SW == 0 {
-                    let next = Point {
+                    Some(Point {
                         x: elf.x - 1,
                         y: elf.y,
-                    };
-                    let e = proposals.entry(next).or_default();
-                    e.push(*elf);
-                    true
+                    })
                 } else {
-                    false
+                    None
                 }
             }
             Self::East => {
                 if neighbors & E_NE_SE == 0 {
-                    let next = Point {
+                    Some(Point {
                         x: elf.x + 1,
                         y: elf.y,
-                    };
-                    let e = proposals.entry(next).or_default();
-                    e.push(*elf);
-                    true
+                    })
                 } else {
-                    false
+                    None
                 }
             }
         }
@@ -104,9 +127,6 @@ pub struct UnstableDiffusion {
 
 impl UnstableDiffusion {
     pub fn rounds(&mut self, num: usize) -> i16 {
-        let mut proposals: FxHashMap<Point, Vec<Point>> =
-            FxHashMap::with_capacity_and_hasher(self.elves.len(), Default::default());
-
         let mut choices = VecDeque::with_capacity(4);
         choices.push_back(Choice::North);
         choices.push_back(Choice::South);
@@ -114,38 +134,70 @@ impl UnstableDiffusion {
         choices.push_back(Choice::East);
 
         for _ in 0..num {
-            for elf in self.elves.iter() {
-                let mut found_neighbors = 0;
-                for (n_idx, (dx, dy)) in NEIGHBORS.iter().enumerate() {
-                    let n = Point {
-                        x: elf.x + dx,
-                        y: elf.y + dy,
-                    };
-                    if self.elves.contains(&n) {
-                        found_neighbors |= 1 << n_idx;
-                    }
-                }
+            let mut next_elves =
+                FxHashSet::with_capacity_and_hasher(self.elves.len(), Default::default());
+            let order = choices[0].order_when_first();
+            let chunks = choices[0].chunks_when_first();
 
-                if found_neighbors == 0 {
-                    continue;
-                }
+            for elf in self.elves.iter() {
+                let mut choice_idxs = order.iter();
+                let mut chunks = chunks.iter();
+                let mut prop: Option<Point> = None;
+                let mut found_neighbors = 0;
 
                 for choice in choices.iter() {
-                    if choice.propose(elf, found_neighbors, &mut proposals) {
+                    for _ in 0..*chunks.next().unwrap() {
+                        let n_idx = *choice_idxs.next().unwrap();
+                        let (dx, dy) = NEIGHBORS[n_idx];
+                        let n = Point {
+                            x: elf.x + dx,
+                            y: elf.y + dy,
+                        };
+                        if self.elves.contains(&n) {
+                            found_neighbors |= 1 << n_idx;
+                        }
+                    }
+
+                    if prop.is_none() {
+                        if let Some(dest) = choice.propose(elf, found_neighbors) {
+                            prop = Some(dest);
+                            // we can only break early when we make a choice if
+                            // we've found at least one neighbor, because this
+                            // might have been the first choice and we need
+                            // to check for others
+                            if found_neighbors > 0 {
+                                break;
+                            }
+                        }
+                    } else if found_neighbors > 0 {
+                        // we have already made a choice in a previous iteration
+                        // and we've found a neighbor, so break early
                         break;
                     }
                 }
-            }
 
-            for (dest, elves) in proposals.drain() {
-                if elves.len() == 1 {
-                    self.elves.remove(&elves[0]);
-                    self.elves.insert(dest);
+                // add the proposal
+                if found_neighbors > 0 {
+                    if let Some(dest) = prop {
+                        if !next_elves.insert(dest) {
+                            next_elves.remove(&dest);
+                            next_elves.insert(*elf);
+                            next_elves.insert(Point {
+                                x: dest.x * 2 - elf.x,
+                                y: dest.y * 2 - elf.y,
+                            });
+                        }
+                        continue;
+                    }
                 }
+
+                next_elves.insert(*elf);
             }
 
             let first = choices.pop_front().unwrap();
             choices.push_back(first);
+
+            self.elves = next_elves;
         }
 
         let mut bounds: Bound2D<i16> = Bound2D::minmax();
@@ -171,9 +223,6 @@ impl UnstableDiffusion {
     }
 
     pub fn rounds_until_no_moves(&mut self) -> usize {
-        let mut proposals: FxHashMap<Point, Vec<Point>> =
-            FxHashMap::with_capacity_and_hasher(self.elves.len(), Default::default());
-
         let mut choices = VecDeque::with_capacity(4);
         choices.push_back(Choice::North);
         choices.push_back(Choice::South);
@@ -184,44 +233,75 @@ impl UnstableDiffusion {
 
         loop {
             count += 1;
+            let mut moved = 0;
+            let mut next_elves =
+                FxHashSet::with_capacity_and_hasher(self.elves.len(), Default::default());
+            let order = choices[0].order_when_first();
+            let chunks = choices[0].chunks_when_first();
 
             for elf in self.elves.iter() {
+                let mut choice_idxs = order.iter();
+                let mut chunks = chunks.iter();
+                let mut prop: Option<Point> = None;
                 let mut found_neighbors = 0;
-                for (n_idx, (dx, dy)) in NEIGHBORS.iter().enumerate() {
-                    let n = Point {
-                        x: elf.x + dx,
-                        y: elf.y + dy,
-                    };
-                    if self.elves.contains(&n) {
-                        found_neighbors |= 1 << n_idx;
-                    }
-                }
-
-                // don't bother checking if we didn't find anyone or we're
-                // completely surrounded
-                if found_neighbors == 0 || found_neighbors == 0b11111111 {
-                    continue;
-                }
 
                 for choice in choices.iter() {
-                    if choice.propose(elf, found_neighbors, &mut proposals) {
+                    for _ in 0..*chunks.next().unwrap() {
+                        let n_idx = *choice_idxs.next().unwrap();
+                        let (dx, dy) = NEIGHBORS[n_idx];
+                        let n = Point {
+                            x: elf.x + dx,
+                            y: elf.y + dy,
+                        };
+                        if self.elves.contains(&n) {
+                            found_neighbors |= 1 << n_idx;
+                        }
+                    }
+
+                    if prop.is_none() {
+                        if let Some(dest) = choice.propose(elf, found_neighbors) {
+                            prop = Some(dest);
+                            // we can only break early when we make a choice if
+                            // we've found at least one neighbor, because this
+                            // might have been the first choice and we need
+                            // to check for others
+                            if found_neighbors > 0 {
+                                break;
+                            }
+                        }
+                    } else if found_neighbors > 0 {
+                        // we have already made a choice in a previous iteration
+                        // and we've found a neighbor, so break early
                         break;
                     }
                 }
-            }
 
-            let mut any_move = false;
-            for (dest, elves) in proposals.drain() {
-                if elves.len() == 1 {
-                    self.elves.remove(&elves[0]);
-                    self.elves.insert(dest);
-                    any_move = true;
+                // add the proposal
+                if found_neighbors > 0 {
+                    if let Some(dest) = prop {
+                        if !next_elves.insert(dest) {
+                            next_elves.remove(&dest);
+                            next_elves.insert(*elf);
+                            next_elves.insert(Point {
+                                x: dest.x * 2 - elf.x,
+                                y: dest.y * 2 - elf.y,
+                            });
+                            moved -= 1;
+                        } else {
+                            moved += 1;
+                        }
+                        continue;
+                    }
                 }
+
+                next_elves.insert(*elf);
             }
 
-            if !any_move {
+            if moved == 0 {
                 break count;
             }
+
+            self.elves = next_elves;
 
             let first = choices.pop_front().unwrap();
             choices.push_back(first);
